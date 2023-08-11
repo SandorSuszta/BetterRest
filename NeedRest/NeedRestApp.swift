a30e6c804c3a3081ca4a643ca0c96a2956c011af
Sandor Suszta <sasha.shusta@gmail.com>
2023-08-11 15:59:50 +0100

    Initial Commit

---
+++ NeedRest/NeedRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeedRestApp.swift
+//  NeedRest
+//
+//  Created by Nataliia Shusta on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct NeedRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
