51a41870d3972a174bd4a4f931ffebb80f3216c3
Sandor Suszta <sasha.shusta@gmail.com>
2023-08-12 18:31:50 +0100

    Implement CoreML functionality

--- NeedRest/ContentView.swift
+++ NeedRest/ContentView.swift
@@ -1,9 +1,14 @@
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUpTime = Date.now
     @State private var cofeeAnmmount = 1
     @State private var sleepAmmount = 8.0
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showAlert = false
     
     var body: some View {
         NavigationStack {
@@ -32,11 +37,35 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateBedtime)
             }
+            .alert(alertTitle, isPresented: $showAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
     
     private func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try RestData(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUpTime)
+            let hours = (components.hour ?? 0) * 60 * 60
+            let minutes = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hours + minutes), coffee: Double(cofeeAnmmount), actualSleep: sleepAmmount)
+            
+            let sleepTime = wakeUpTime - prediction.estimatedSleep
+            alertTitle = "Your ideal sleep time is:"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        }
+        catch {
+            alertTitle = "Error"
+            alertMessage = "Something went wrong"
+        }
         
+        showAlert = true
     }
 }
 
